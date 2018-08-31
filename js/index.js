5826f72e65dd5abfe9ee403a4f2fbf8e569638e1
san1j <sanjanasreenath96@gmail.com>
2018-08-31 15:16:16 -0400

    use p5.Speech instead of input field

--- js/index.js
+++ js/index.js
@@ -1,33 +1,50 @@
-var input;
-var letters=[];
+var words=[];
+var foo = new p5.SpeechRec();
+foo.onResult = letterBubbles; 
+foo.continuous = true;
+foo.start(); 
 
 function setup() { 
   createCanvas(windowWidth,windowHeight);
-  input = createInput();
   textFont("Times");
-  input.position(100,height-40);
   background(0);
+  frameRate(18);
+  ellipseMode(CENTER);
+  let h2 = createElement('h2','Start Speaking!!');
+  h2.position(10,10)
 } 
 
 function bubble() { 
- translate(random(width),random(height));
-	for (var i = 0; i < letters.length; i++) {
-		push();
-		rotate(TWO_PI * i / letters.length+6);
-    textSize(18);
-    fill(255);
-		text(letters[i],letters.length+35,0);
-		pop();
+    words.forEach(letter=>{
+    push();      
+    translate(letter[letter.length-2],letter[letter.length-1]);
+    rotate(radians(frameCount));
+   	for (var i = 0; i < letter.length-2; i++) {
+        push();
+        translate(0,0);
+		rotate(TWO_PI * i / letter.length+20);
+        textSize(18);
+        fill(220,20,60);
+        text(letter[i],letter.length+40,0);
+        pop();
 	}
-  
+    pop();
+  })
 }
 
-function keyPressed() {
-    if(keyCode == ENTER || keyCode == RETURN) {
-        var inputVal = input.value();
-        letters = inputVal.split("");
-        bubble();
-        input.value('');
-    }
-   
+function draw(){
+   background(0)
+   bubble();
+}
+
+
+function letterBubbles(){
+    console.log(foo.resultString)
+     var letters = foo.resultString.split("");
+     var  x =  random(50,width-50)
+     var  y =  random(50,height-50)
+     letters.push(x);
+     letters.push(y);
+     words.push(letters)
+     bubble();
 }
